a9521467d59e5ce0230f54c8f9d24628405f1116
unknown <fuaad29@gmail.com>
2024-05-29 18:04:54 +0500

    tailwindcss installed

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 function Home() {
   return(
     <>
-      <h1>Welcome To My Portfolio Website</h1>
+      <h1 className="">Welcome To My Portfolio Website</h1>
 
       <div><Link to="/calculator">Calculator</Link></div>
-      <div><Link to="/phonebook">Phonebook App</Link></div>
-      <div><Link to="/calculator">Calculator</Link></div>
+      <div className=""><Link to="/phonebook">Phonebook App</Link></div>
+      <div className=""><Link to="/calculator">Calculator</Link></div>
       <div><Link to="/counter">Counter</Link></div>
 
     </>
