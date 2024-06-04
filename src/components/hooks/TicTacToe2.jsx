1980ce9db862a1681ed3281fc10dfc180cb2bfa9
unknown <fuaad29@gmail.com>
2024-06-04 18:50:31 +0500

    modified code

--- src/components/hooks/TicTacToe2.jsx
+++ src/components/hooks/TicTacToe2.jsx
@@ -5,6 +5,8 @@ function TicTacToe2() {
   const [state, setState]  = useState(Array(9).fill(null))
   const [firstPlayersTurn, setFirstPlayersTurn] = useState(true)
   const [winner, setWinner] = useState(null)
+
+  console.log("state:", state);
   const handleClick = (index) => {
     // console.log(index);
     const newState = [...state];
@@ -13,14 +15,15 @@ function TicTacToe2() {
     setFirstPlayersTurn(!firstPlayersTurn)
   }
 
-  const winnerLogicFunction = () => {
-    const length = 3
-    const arr = []
-    for (let i=0; i<length; i++) {
-      arr.push(i)
-      console.log("arr:", arr);
-    }
-  }
+  // const winnerLogicFunction = () => {
+  //   const length = 3
+  //   const arr = []
+  //   for (let i=0; i<length; i++) {
+  //     arr.push(i)
+  //     console.log("arr:", arr);
+  //   }
+  // }
+
   const checkWinner = () => {
     const winnerLogic = [[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8], [0,4,8], [2,4,6], [0,1,2]]
   
@@ -42,29 +45,29 @@ function TicTacToe2() {
     checkWinner();
   }, [state]);
   return (
-    
     <div>
-      {winner !== null ? <>{winner} Won</> :
-      <>
-      <div className="flex">
-        <Square onClick={()=>{handleClick(0)}} value={state[0]}/>
-        <Square onClick={()=>{handleClick(1)}} value={state[1]}/>
-        <Square onClick={()=>{handleClick(2)}} value={state[2]}/>
-      </div>
-      <div className="flex">
-        <Square onClick={()=>{handleClick(3)}} value={state[3]}/>
-        <Square onClick={()=>{handleClick(4)}} value={state[4]}/>
-        <Square onClick={()=>{handleClick(5)}} value={state[5]}/>
-      </div>
-      <div className="flex">
-        <Square onClick={()=>{handleClick(6)}} value={state[6]}/>
-        <Square onClick={()=>{handleClick(7)}} value={state[7]}/>
-        <Square onClick={()=>{handleClick(8)}} value={state[8]}/>
-      </div>
-      </>}
+      {
+        winner !== null ? <>{winner} Won</> :
+        <>
+        <div className="flex">
+          <Square onClick={()=>{handleClick(0)}} value={state[0]}/>
+          <Square onClick={()=>{handleClick(1)}} value={state[1]}/>
+          <Square onClick={()=>{handleClick(2)}} value={state[2]}/>
+        </div>
+        <div className="flex">
+          <Square onClick={()=>{handleClick(3)}} value={state[3]}/>
+          <Square onClick={()=>{handleClick(4)}} value={state[4]}/>
+          <Square onClick={()=>{handleClick(5)}} value={state[5]}/>
+        </div>
+        <div className="flex">
+          <Square onClick={()=>{handleClick(6)}} value={state[6]}/>
+          <Square onClick={()=>{handleClick(7)}} value={state[7]}/>
+          <Square onClick={()=>{handleClick(8)}} value={state[8]}/>
+        </div>
+        </>
+      }
     </div>
   )
-  
 }
 
 function Square({ onClick, value }) {
